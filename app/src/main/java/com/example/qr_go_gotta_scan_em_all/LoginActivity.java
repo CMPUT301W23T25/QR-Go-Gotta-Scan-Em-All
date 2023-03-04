028842fe2dc22e8e31c49ddf001b9bfb6fabf9ea
nDevised <90827405+nDevised@users.noreply.github.com>
2023-03-04 05:03:18 -0700

    Improved Navigation bar and added listner

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/LoginActivity.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/LoginActivity.java
@@ -2,13 +2,21 @@ package com.example.qr_go_gotta_scan_em_all;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+    // Declare method to switch to LoginActivity
+    private void switchToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // TEMPORARY code No DB Set up yet
+        switchToMainActivity();
     }
 }
