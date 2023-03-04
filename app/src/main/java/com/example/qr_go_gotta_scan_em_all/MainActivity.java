a2c0036d12e88e431a3e68d763a3f327c28bba95
nDevised <90827405+nDevised@users.noreply.github.com>
2023-03-04 04:26:45 -0700

    Added Navigation bar

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/MainActivity.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.qr_go_gotta_scan_em_all;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    // https://github.com/hamidsaid/Modern-Bottom-Navigation/tree/main/app/src
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView btmNavView = findViewById(R.id.btmNavView);
+        btmNavView.setItemTextColor(ColorStateList.valueOf(Color.RED));
     }
 }
