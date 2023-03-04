24e7097fc81665da83329404d704d3d9d6563087
Hrishi Shah <shrishi291002@gmail.com>
2023-03-04 15:49:16 -0700

    Made GUI for View Profile and Create Profile Page

---
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.qr_go_gotta_scan_em_all;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+    }
+}
