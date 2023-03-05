94b7a453836768e069faf0f36a7f7660af995878
nDevised <90827405+nDevised@users.noreply.github.com>
2023-03-04 17:19:16 -0700

    Added phoneID getter

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/LoginActivity.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/LoginActivity.java
@@ -1,19 +1,37 @@
 package com.example.qr_go_gotta_scan_em_all;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.telephony.TelephonyManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     // Declare method to switch to LoginActivity
+    TextView userText;
+    String phoneID;
+    String userName;
+    DeviceIdHelper dIDMan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        dIDMan = new DeviceIdHelper(this);
+        phoneID = dIDMan.getDeviceId();
 
-        // TEMPORARY code No DB Set up yet
-  /*      switchToMainActivity();*/
+    }
+
+
+    // Create user session function
+    private void createUserSession(){
+        // Firstly check the the database if the phone ID is registered or not.
+
+    }
+
+    private void switchToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
