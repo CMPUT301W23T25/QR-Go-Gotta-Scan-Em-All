ac8ed3a281fecb7e2e829f43d5bbecf90194216d
shaan965 <73771504+shaan965@users.noreply.github.com>
2023-03-07 21:09:06 -0700

    Updated QR code scanner

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/QrScanner.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/QrScanner.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
@@ -18,7 +19,7 @@ import com.google.zxing.Result;
 
 public class QrScanner extends AppCompatActivity {
     private CodeScanner mCodeScanner;
-    private Button back_btn;
+    private ImageView back_btn;
 
 
     @Override
