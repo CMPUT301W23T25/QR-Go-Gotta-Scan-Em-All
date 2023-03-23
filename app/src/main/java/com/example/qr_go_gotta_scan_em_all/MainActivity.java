03a7747a788466c5676d4cda2c3a23db30f1742f
nDevised <90827405+nDevised@users.noreply.github.com>
2023-03-22 19:21:40 -0600

    Revert "Merge branch 'main' into reverted-nDevised2"
    This reverts commit 999008eac47eaceeee0af7b8ac20441953ee0e48, reversing
    changes made to c38abc095b7c02aea419ff9fca6400d317e54f29.

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/MainActivity.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.qr_go_gotta_scan_em_all;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -15,15 +18,30 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.util.ScopeUtil;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -83,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
             Pokemon pokemonAdded;
             if (result.getData() != null && result.getResultCode() == RESULT_OK) {
                 pokemonAdded = (Pokemon) result.getData().getSerializableExtra("pokemon");
-                player.addPokemon(pokemonAdded);
+                player.addPokemonToArray(pokemonAdded);
                 goToOverview();
 
             }
@@ -112,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         handleNavBar();
         handlePokeBall();
 
-        player.addPokemon(new Pokemon("test1"));
+        player.addPokemonToArray(new Pokemon("test1"));
 
 
     }
@@ -168,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(pkRaw);
         if (pkRaw != null){
             Pokemon pk = (Pokemon)pkRaw;
-            player.addPokemon(pk);
+            player.addPokemonToArray(pk);
             System.out.println("NICEEEEE");
         }
 
