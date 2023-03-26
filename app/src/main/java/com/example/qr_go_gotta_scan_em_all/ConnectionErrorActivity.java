b6d97666a0a4b0fd13fbee1c471cc6ae4d701703
shaan965 <73771504+shaan965@users.noreply.github.com>
2023-03-26 01:45:11 -0600

    Updated connectionError with Try Again button

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/ConnectionErrorActivity.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/ConnectionErrorActivity.java
@@ -2,9 +2,12 @@ package com.example.qr_go_gotta_scan_em_all;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-        /**
+/**
         * A simple {@link ConnectionErrorActivity} subclass.
         * Use the {@link AppCompatActivity} factory method to
         * create an instance of this class.
@@ -12,9 +15,24 @@ import android.os.Bundle;
         */
 public class ConnectionErrorActivity extends AppCompatActivity {
 
+    private Intent try_again_attempt;
+    private ImageView try_again_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection_error);
+        try_again_attempt = new Intent(ConnectionErrorActivity.this, LoginActivity.class);
+        try_again_btn = findViewById(R.id.try_again_button);
+
+        try_again_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(try_again_attempt);
+            }
+        });
+    
     }
+
+
 }
