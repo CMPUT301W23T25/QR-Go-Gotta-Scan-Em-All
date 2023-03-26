090bc747e86a18943d641c338c546c1ae7b45f31
nDevised <90827405+nDevised@users.noreply.github.com>
2023-03-26 05:51:48 -0600

    Work in progress of comments

---
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/CommentsArrayAdapter.java
@@ -0,0 +1,80 @@
+package com.example.qr_go_gotta_scan_em_all;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+/**
+
+ This class is an ArrayAdapter used to display a list of Pokemon objects in a ListView.
+ It extends the ArrayAdapter class and overrides the getView method to populate each row of the ListView with the appropriate Pokemon object properties.
+ */
+
+
+public class CommentsArrayAdapter extends ArrayAdapter<Comment> {
+
+    /**
+     * Constructor for PokemonArrayAdapter.
+     * @param context The context of the activity or fragment where the adapter will be used.
+     * @param objects An ArrayList of Pokemon objects to be displayed in the ListView.
+     */
+    public CommentsArrayAdapter(@NonNull Context context, @NonNull ArrayList<Comment> objects) {
+        super(context, 0, objects);
+    }
+
+    /**
+     * This method overrides the getView method in ArrayAdapter to populate each row of the ListView with the appropriate Pokemon object properties.
+     * @param position The position of the Pokemon object in the ArrayList that we want to display in the current row of the ListView.
+     * @param convertView A view that has been inflated from a previous row in the ListView. This view can be recycled and reused to save memory.
+     * @param parent The parent ViewGroup that the current row of the ListView belongs to.
+     * @return A view that represents a row in the ListView.
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        // convertView is used for converting the information from the front end (view)
+        // to backend
+
+        View view;
+        if(convertView == null){
+            // if there is no previously used view for the fragment, then the addgasstation layout
+            // must be inflated
+            // this is for optimization so that the previously used view can be used.
+            view = LayoutInflater.from(getContext()).inflate(R.layout.content2,parent,false);
+        } else {
+            view = convertView;
+        }
+        // Get the item from the array adapter that we want to project onto our listview
+        Comment c = super.getItem(position);
+
+        // get all the components from the GasStation object that we want on the listview
+
+        TextView userNameTw = view.findViewById(R.id.user_name);
+        TextView textTw = view.findViewById(R.id.comment_text);
+        
+
+        // Name
+        String userName = c.getPlayer().getUserName();
+        // Text
+        String text = c.getText();
+
+        // Credits: Android Studio Website
+        // Reference: https://developer.android.com/reference/java/text/SimpleDateFormat
+
+        // Simple date format requires date object
+
+        userNameTw.setText("Name: "+userName);
+        textTw.setText("Comment: " + text);
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/QRMoreInfoFragment.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/QRMoreInfoFragment.java
@@ -1,12 +1,29 @@
 package com.example.qr_go_gotta_scan_em_all;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +40,7 @@ public class QRMoreInfoFragment extends Fragment {
     private Player p;
     private Database db;
     private Pokemon pk;
+    private ArrayList<Comment> comments;
 
     // TODO: Rename and change types of parameters
 
@@ -35,6 +53,7 @@ public class QRMoreInfoFragment extends Fragment {
         // Needed for checking if the pokemon the player is commenting on is actually in the array or not.
         this.p = p;
         this.pk = pk;
+        this.comments = new ArrayList<>();
     }
 
 
@@ -50,5 +69,142 @@ public class QRMoreInfoFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_q_r_more_info, container, false);
     }
+
+    private void getCommentsFromDB(){
+//        db.getPokemonCol().document(pk.getID()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = task.getResult();
+//                    if (document.exists()) {
+//
+//                        List<Map<String, String>> myArray = (List<Map<String, String>>) document.get("comments");
+//
+//
+//                        // Store the array of hashmaps of the pokemons  { pokemonID: …, location: …, photo: …, }.
+//
+//                    }
+//
+//                } else {
+//                    startActivity(new Intent(((MainActivity)getActivity()), ConnectionErrorActivity.class));
+//                }
+//            }
+//        });
+
+// Query Pokemon collection to get comments for the specified Pokemon
+        db.getPokemonCol()
+                .document(pk.getID())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                // Get comments field from Pokemon document
+                                List<Map<String, String>> commentsM = (List<Map<String, String>>) document.get("comments");
+                                if (comments != null) {
+                                    // Query players collection to get usernames
+                                    for (Map<String, String> c : commentsM) {
+                                        String userId = c.get("user_id");
+                                        db.getPlayerCol()
+                                                .document(userId)
+                                                .get()
+                                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                        if (task.isSuccessful()) {
+                                                            DocumentSnapshot document = task.getResult();
+                                                            if (document.exists()) {
+                                                                String username = document.getString("user_name");
+                                                                // Add username to comment
+                                                                c.put("user_name", username);
+                                                            } else {
+                                                                Log.d(TAG, "No such document");
+                                                            }
+                                                        } else {
+                                                            Log.d(TAG, "get failed with ", task.getException());
+                                                        }
+                                                    }
+                                                });
+                                        // Add the comment to the array of comments
+                                        Comment comment = new Comment(new Player((String)c.get("user_id"),(String)c.get("user_name")),(String)c.get("text"));
+                                        comments.add(comment);
+                                    }
+                                    // Do something with comments array after all queries complete
+                                    // For example, update UI with comments and usernames
+                                }
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+
+    }
+
+//    private ArrayList<PokemonInformation> convertRawDataToPInfo(List<Map<String,Object>> a){
+//        // Make a new array
+//
+//        ArrayList<Comment> pIList = new ArrayList<>();
+//        for (Map<String,Object> m:a){
+////            PokemonInformation pI = new PokemonInformation();
+//            Comment c = new Comment(new Player(),);
+//            Pokemon p= new Pokemon();
+//            p.setID((String)m.get("ID"));
+//        }
+//
+//        return pIList;
+//
+//    }
+
+    /**
+     *
+     *
+     * This method retrieves the player data from the database based on their login
+     * information,
+     * creates a Player object, and sets the "isRegistered" flag to true if the
+     * player is found in
+     * the database. It also switches to the main activity and passes the player
+     * object as an extra
+     * to the intent.
+     */
+
+//    private void getPlayerData(String userID, Player player) {
+//
+//
+//        db.getPlayerCol().document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = task.getResult();
+//                    if (document.exists()) {
+//                        player = new Player((String)document.get("username"), document.getId());
+//                        List<Map<String, Object>> myArray = (List<Map<String, Object>>) document.get("pokemon_owned");
+//                        player.setPokemonArray(convertRawDataToPInfo(myArray));
+//                        //
+//                        isRegistered = true;
+//                        System.out.println("registered");
+//                        intent.putExtra("player",player);
+//
+//                        // Store the array of hashmaps of the pokemons  { pokemonID: …, location: …, photo: …, }.
+//
+//                        switchToMainActivity();
+//
+//                    } else {
+//                        System.out.println("tre");
+//                        isRegistered = false;
+//                    }
+//
+//                } else {
+//                    switchToNetworkFail();
+//                }
+//            }
+//        });
+//
+//    }
 
 }
