a9f56d7fafddff54c620ad28178a85e16b76258d
Farel <farellukas@gmail.com>
2023-04-01 02:39:48 -0600

    Created fragment for player search

---
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/PlayerSearchFragment.java
@@ -0,0 +1,25 @@
+package com.example.qr_go_gotta_scan_em_all;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayerSearchFragment extends Fragment {
+
+    public PlayerSearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_player_search, container, false);
+
+        return view;
+    }
+}
