6439f1d9f5016a649ffdb2d2a50062f675f458e1
nDevised <90827405+nDevised@users.noreply.github.com>
2023-04-03 11:01:43 -0600

    Started new ASCII

--- app/src/main/java/com/example/qr_go_gotta_scan_em_all/Pokemon.java
+++ app/src/main/java/com/example/qr_go_gotta_scan_em_all/Pokemon.java
@@ -108,6 +108,358 @@ public class Pokemon implements Serializable {
         return name;
     }
 
+    public String visualReperN(){
+        List<List<String>> nameList = new ArrayList<List<String>>();
+
+        // There are 20 possible different pokemon bodies
+        nameList.add(Arrays.asList(
+                "      .\"`\".\n",
+                "  .-./ _=_ \\.-.\n",
+                " {  },(oYo),{ }}\n",
+                " {{ |   \"   |} }\n",
+                " { { \\(---)/  }}\n",
+                " {{  }'-=-'{ } }\n",
+                " { { }._:_.{  }}\n",
+                " {{  } -:- { } }\n",
+                " {_{ }`===`{  _}\n",
+                "((((\\)     (/))))\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                " /\\                 /\\\n",
+                "/ \\'._   (\\_/)   _.'/ \\\n",
+                "|.''._'--(o.o)--'_.''.|\n",
+                " \\_ / `;=/ \" \\=;` \\ _/\n",
+                "   `\\__| \\___/ |__/`\n",
+                "        \\(_|_)/\n",
+                "         \" ` \"\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                " /=\\\"\"/=\\\n",
+                "|=(0_0)=|__\n",
+                " \\_\\ _/_/   )\n",
+                "  /_/   _  /\\\n",
+                "  | /|\\ || |\n",
+                "    ~ ~  ~\n"
+        ));
+        nameList.add(Arrays.asList(
+                "  /.--.\\  .-.  /.||.\\\n",
+                " /.,   \\\\(0.0)// || \\\\\n",
+                "/;`\";/\\ \\\\|m|//  ||  ;\\\n",
+                "|:   \\ \\__`:`____||__:|\n",
+                "|:    \\__ \\T/ (@~)(~@)|\n",
+                "|:    _/|     |\\_\\/  :|\n",
+                "|:   /  |     |  \\   :|\n",
+                "|'  /   |     |   \\  '|\n",
+                " \\_/    |_____|    \\_/\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "(\\_/) \n",
+                "(◕‿◕)  . / \\\n",
+                "/ \" \\. /'.|\n",
+                "\\___/\\ _/\n",
+                "(_|_)/\n",
+                "\" ` \"  \n"
+                ));
+
+        nameList.add(Arrays.asList(
+                "        ___\n",
+                "       (. .)\n",
+                "   ,-.(.___.),-.\n",
+                "  ( \\ \\ '--' / / )\n",
+                "   \\ \\ / ,. \\ / /\n",
+                "    ) '| || |' ( mrf\n",
+                "OoO'- OoO''OoO -'OoO\n"
+        ));
+        nameList.add(Arrays.asList(
+                "     _ _\n",
+                "    (oTo)\n",
+                " _.-( _ )-._\n",
+                "`/`( '-' )`\\`\n",
+                "   /'---'\\\n",
+                " __\\     /__\n",
+                " \\_/     \\_/\n"
+        ));
+        nameList.add(Arrays.asList(
+                "   /\\`.   ,'/\\\n",
+                "  //\\\\(0\"0)//\\\\\n",
+                "//     ,^.    \\\\\n",
+                "\\\\            //\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "  /^ ^\\\n",
+                " /(0 0)\\\n",
+                " V\\ Y /V\n",
+                "  / - \\\n",
+                " /    |\n",
+                "V__) ||\n"
+        ));
+        nameList.add(Arrays.asList(
+                "  |_|\n",
+                " (o o)\n",
+                " /===\\\n",
+                "(/===\\)\n",
+                "  \\_/\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "     (()__(()\n",
+                "     /       \\ \n",
+                "    ( /    \\  \\\n",
+                "     \\(o o)   /\n",
+                "     (_()_)__/ \\\n",
+                "    / _,==.____ \\\n",
+                "   (   |--|      )\n",
+                "   /\\_.|__|'-.__/\\_\n",
+                "  / (        /     \\\n",
+                "  \\  \\      (      /\n",
+                "   )  '._____)    /    \n",
+                "(((____.--(((____/m\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                " /\\/\\\n",
+                "(o.o)\n",
+                " >^<\n",
+                "/ \" \\. /'.|\n",
+                "\\___/\\ _/\n",
+                "(_|_)/\n",
+                "\" ` \" \n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "  ^~^  ,\n",
+                " (◕Y◕) )\n",
+                " /   \\/ \n",
+                "(\\|||/) \n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "((.))\n",
+                "(o o)\n",
+                " \\ /\n",
+                "  ^ \n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "   | \\--/ |\n",
+                "   \\ (0_0)|\n",
+                "    \\==Y==/\n",
+                "    /'-\"-'>\n",
+                "  _/ < ; (;\n",
+                " / ,_ |_|_\\\n",
+                "( _,,)\\,,),)\n",
+                "\\ '.___\n",
+                " '-----'\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "//\\\\\n",
+                "|^ ^|\n",
+                "(O|O)\n",
+                "|  ~ \n",
+                "\\ O /\n",
+                " | |\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "   .,--.\n",
+                " .' __  \\\n",
+                " | .._  |\n",
+                " |(} {)|\n",
+                " / /|  |.\n",
+                "(_/ /____)\n",
+                "  |_||\n",
+                "    /'\n",
+                "    //\n",
+                "  .'''\\\n",
+                " /\\:::/\\\n",
+                "( /|::|\\\\\n",
+                "_\\:|;;|{/_\n",
+                "'.;|**|\\;,/\n",
+                "   \\_ /\n",
+                "   | ||\n",
+                "   | ||\n",
+                "   | ||\n",
+                "   | ||\n",
+                " ._| ||_.\n",
+                ";,_.-._,;\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                " /----(  ^_^  )----\\\n",
+                "|  {   \\('v')/   }  |\n",
+                "|   {   /   \\   }   |\n",
+                "|_)(   /\\   /\\   )(_|\n",
+                "|)  (_ | \\|/  |_)  (|\n",
+                "'     \"--^^^^--\"    '\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                " /\\_/\\\n",
+                "/(o o)\\\n",
+                "\\     /\n",
+                " )   (\n",
+                "/ / \\ \\\n",
+                "(_) (_)\n"
+        ));
+
+        nameList.add(Arrays.asList(
+                "{} {}\n",
+                "(.Y.)\n",
+                " \\_/\n",
+                "/|||\\\n",
+                "\\|||/\n",
+                "i| |i\n",
+                "`` ``\n"
+        ));
+
+        String hat =" [```]\n=======\n";
+        String glasses = "(◪-◪)";
+
+        // Determine the Pokemon
+        char first = ID.charAt(0);
+        char second = ID.charAt(1);
+        char third = ID.charAt(2);
+        char fourth = ID.charAt(3);
+        char fifth = ID.charAt(4);
+        char sixth = ID.charAt(5);
+        boolean isHat = false;
+        boolean isGlasses = false;
+        List<String> pokemon;
+        String name = "";
+
+        // Mega or not
+        if (third == '0' || third == '1' || third == '2' || third == '3' ||
+                third == '4' || third == '5' || third == '6' || third == '7') {
+            name += "Mega ";
+        } else{
+
+        }
+
+        // Get the class of Pokemon firstly by checking the first digit of the ID
+        // 0, 1, 2, 3 = Normal
+        // 4, 5, 6, 7 = Electric
+        // 8, 9, a, b = Ghost
+        // c, d, e, f = Water
+
+        List<String> normalPokemon = Arrays.asList("Meowth", "Evee", "Snorlax", "Ditto", "Kangaskhan", "Tauros", "Bidoof", "Null");
+        List<String> electricPokemon = Arrays.asList("Pikachu", "Raichu", "Jolteon", "Raikou", "Bellibolt", "Electrode", "Electivire", "Zeraora");
+        List<String> ghostPokemon = Arrays.asList("Gengar", "Dusknoir", "Cofagrigus", "Sinistea", "Spectrier", "Giratina", "Palossand", "Mimikyu");
+        List<String> waterPokemon = Arrays.asList("Blastoise", "Psyduck", "Golduck", "Vaporeon", "Wailord", "Quaxly", "Wiglett", "Gyarados");
+
+        if (first == '0' || first == '1' || first == '2' || first == '3'){
+            // Handle Normal Names
+            if (second == '0' || second == '1') {
+                pokemon = nameList.get(0);
+            } else if (second == '2' || second == '3') {
+                pokemon = nameList.get(0);
+            } else if (second == '4' || second == '5') {
+                pokemon = nameList.get(1);
+            } else if (second == '6' || second == '7') {
+                pokemon = nameList.get(1);
+            } else if (second == '8' || second == '9') {
+                pokemon = nameList.get(2);
+            } else if (second == 'a' || second == 'b') {
+                pokemon = nameList.get(2);
+            } else if (second == 'c' || second == 'd') {
+                pokemon = nameList.get(3);
+            } else if (second == 'e' || second == 'f') {
+                pokemon = nameList.get(3);
+            }
+
+        } else if (first == '4' || first == '5' || first == '6' || first == '7'){
+            // Handle Electric names
+            if (second == '0' || second == '1') {
+                pokemon = electricPokemon.get(0);
+            } else if (second == '2' || second == '3') {
+                pokemon = electricPokemon.get(1);
+            } else if (second == '4' || second == '5') {
+                pokemon = electricPokemon.get(2);
+            } else if (second == '6' || second == '7') {
+                pokemon = electricPokemon.get(3);
+            } else if (second == '8' || second == '9') {
+                pokemon = electricPokemon.get(4);
+            } else if (second == 'a' || second == 'b') {
+                pokemon = electricPokemon.get(5);
+            } else if (second == 'c' || second == 'd') {
+                pokemon = electricPokemon.get(6);
+            } else if (second == 'e' || second == 'f') {
+                pokemon = electricPokemon.get(7);
+            }
+        } else if (first == '8' || first == '9' || first == 'a' || first == 'b'){
+            // Handle Ghost names
+            if (second == '0' || second == '1') {
+                pokemon = ghostPokemon.get(0);
+            } else if (second == '2' || second == '3') {
+                pokemon = ghostPokemon.get(1);
+            } else if (second == '4' || second == '5') {
+                pokemon = ghostPokemon.get(2);
+            } else if (second == '6' || second == '7') {
+                pokemon = ghostPokemon.get(3);
+            } else if (second == '8' || second == '9') {
+                pokemon = ghostPokemon.get(4);
+            } else if (second == 'a' || second == 'b') {
+                pokemon = ghostPokemon.get(5);
+            } else if (second == 'c' || second == 'd') {
+                pokemon = ghostPokemon.get(6);
+            } else if (second == 'e' || second == 'f') {
+                pokemon = ghostPokemon.get(7);
+            }
+        } else{
+            // Handle Water names
+            if (second == '0' || second == '1') {
+                pokemon = waterPokemon.get(0);
+            } else if (second == '2' || second == '3') {
+                pokemon = waterPokemon.get(1);
+            } else if (second == '4' || second == '5') {
+                pokemon = waterPokemon.get(2);
+            } else if (second == '6' || second == '7') {
+                pokemon = waterPokemon.get(3);
+            } else if (second == '8' || second == '9') {
+                pokemon = waterPokemon.get(4);
+            } else if (second == 'a' || second == 'b') {
+                pokemon = waterPokemon.get(5);
+            } else if (second == 'c' || second == 'd') {
+                pokemon = waterPokemon.get(6);
+            } else if (second == 'e' || second == 'f') {
+                pokemon = waterPokemon.get(7);
+            }
+        }
+
+        name += pokemon;
+
+        // Cap or no cap
+        if (fifth == '0' || fifth == '1' || fifth == '2' || fifth == '3' ||
+                fifth == '4' || fifth == '5' || fifth == '6' || fifth == '7') {
+            name += " with Cap";
+            isHat = true;
+        }
+        // Sunglasses or no sunglasses
+        if (sixth == '0' || sixth == '1' || sixth == '2' || sixth == '3' ||
+                sixth == '4' || sixth == '5' || sixth == '6' || sixth == '7') {
+            if(isHat){
+                name += " and Glasses";
+            } else{
+                name += " with Glasses";
+            }
+            isGlasses = true;
+        }
+
+        if (isGlasses && isHat){
+            // Sigma or not
+            if (fourth == '0' || fourth == '1' || fourth == '2' || fourth == '3' ||
+                    fourth == '4' || fourth == '5' || fourth == '6' || fourth == '7') {
+                name += " **Sigma**";
+            }
+        }
+        return name;
+
+    }
+
 
 
 
